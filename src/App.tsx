1f131fd7bbb97f3a19721754eb685ec322a6a49a
onelaht <covercoverunder@gmail.com>
2025-11-08 13:02:40 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
-import './App.css'
+import {MapContainer, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
+import {useEffect} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('unknown')
 
-  return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-        <a href='https://workers.cloudflare.com/' target='_blank'>
-          <img src={cloudflareLogo} className='logo cloudflare' alt='Cloudflare logo' />
-        </a>
-      </div>
-      <h1>Vite + React + Cloudflare</h1>
-      <div className='card'>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label='increment'
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className='card'>
-        <button
-          onClick={() => {
-            fetch('/api/')
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name))
-          }}
-          aria-label='get name'
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    const BASE = import.meta.env.DEV ? '' : 'https://buoymap.onelaht.workers.dev/'
+
+    const fetchTest = async () => {
+        const res = await fetch(`${BASE}/api/stations/`)
+        console.log(res);
+    }
+
+    // useEffect(() => {
+    //     fetchTest();
+    // }, [])
+
+    return (
+            <MapContainer center={[51, -0]} zoom={13} style={{width: "100vw", height: "100vh"}}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+    )
 }
-
-export default App
--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,17 @@
 export default {
-  fetch(request) {
+    async fetch(request) {
     const url = new URL(request.url);
+    const method = request.method;
+    const path = url.pathname;
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+    if(path == "/api/stations/" && method == "GET") {
+        const res = await fetch("https://www.ndbc.noaa.gov/data/stations/station_table.txt")
+        const text = await res.text();
+        return new Response(text, {
+            status: 200,
+            headers: {"Context-Type": "text/plain"}
+        });
     }
-		return new Response(null, { status: 404 });
-  },
+    return new Response(null, { status: 404 });
+  }
 } satisfies ExportedHandler<Env>;
