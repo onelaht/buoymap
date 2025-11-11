d965041f5f7156e6e25440be89da3aeef1661889
onelaht <covercoverunder@gmail.com>
2025-11-11 08:48:20 -0500

    Remove placeholder endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,8 @@ import {useEffect} from "react";
 
 
 export default function App() {
+    // fetching via api
     const BASE = import.meta.env.DEV ? '' : 'https://buoymap.onelaht.workers.dev/'
-
-    const fetchTest = async () => {
-        const res = await fetch(`${BASE}/api/stations/`)
-        console.log(res);
-    }
-
-    // useEffect(() => {
-    //     fetchTest();
-    // }, [])
 
     return (
             <MapContainer center={[51, -0]} zoom={13} style={{width: "100vw", height: "100vh"}}>
