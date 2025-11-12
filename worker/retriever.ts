fe740f1b0873b81a2deddda607ee7696127c0034
onelaht <covercoverunder@gmail.com>
2025-11-12 17:43:05 -0500

    Normalize location metadata per station

--- worker/retriever.ts
+++ worker/retriever.ts
@@ -1,5 +1,6 @@
 // db access
 import {stations} from "./db/stations.ts";
+// types
 import type {StationTableTuple} from "../types/StationTable.ts";
 
 export function retriever(db:D1Database) {
@@ -7,17 +8,23 @@ export function retriever(db:D1Database) {
     const db_stations = stations(db);
 
     // extract stations and normalize location
-    async function retrieveStations() {
-        //
+    async function retrieveStations():Promise<StationTableTuple[]> {
+        // get all tuples from station table
         const stations:StationTableTuple[] = await db_stations.getAll();
+        // traverse through all stations and normalize location
         stations.forEach((s) => {
-            console.log(s);
+            s.location = normalizeLocation(s.location as string);
         })
+        return stations;
     }
 
-// ***********************
-// helpers
-// ***********************
+    // convert location to lat/lon
+     function normalizeLocation(location:string):number[] {
+        const splitData = location.split(" ");
+        const lat = parseFloat(splitData[0]);
+        const lon = parseFloat(splitData[2]);
+        return [splitData[1] === "N" ? lat : -lat, splitData[3] === "E" ? lon : -lon]
+    }
 
     return {retrieveStations};
 }
