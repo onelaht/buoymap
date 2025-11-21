ca55e1f051fc49a91bc5f7a11d37374171fa11b2
onelaht <covercoverunder@gmail.com>
2025-11-21 11:15:35 -0500

    Move provider interface to separate file

---
+++ src/interfaces/IStationsContext.ts
@@ -0,0 +1,17 @@
+import React from "react";
+import type {StationTable} from "../../types/StationTable.ts";
+
+export interface IStationsContext {
+    stations: StationTable[];
+    setStations: React.Dispatch<React.SetStateAction<StationTable[]>>;
+    uniqueCountries: Array<StationTable["country_code"]>;
+    setUniqueCountries: React.Dispatch<React.SetStateAction<Array<StationTable["country_code"]>>>;
+    uniqueOwners: Array<Pick<StationTable, "code" | "owner_name">>;
+    setUniqueOwners: React.Dispatch<React.SetStateAction<Array<Pick<StationTable, "code" | "owner_name">>>>;
+    selCountries: Map<StationTable["country_code"], boolean>;
+    setSelCountries: React.Dispatch<React.SetStateAction<Map<StationTable["country_code"], boolean>>>;
+    selOwners: Map<StationTable["owner_name"], boolean>;
+    setSelOwners: React.Dispatch<React.SetStateAction<Map<StationTable["owner_name"], boolean>>>;
+    filteredStations: StationTable[];
+    setFilteredStations: React.Dispatch<React.SetStateAction<StationTable[]>>
+}
